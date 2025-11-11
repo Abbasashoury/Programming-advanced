2c66e10a2bf9ac6476f85cc13a4cb5ecf35baca4
Abbas Ashoury <abbasashoury84@gmail.com>
2025-11-12 01:10:14 +0330

    final develop "Dooz"

--- Code/Project/miniproject/miniproject2/Dooz.cpp
+++ Code/Project/miniproject/miniproject2/Dooz.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 class Player
 {
-
 private:
     string player;
     char player_sign;
@@ -39,31 +38,31 @@ public:
     bool checkWin()
     {
         for (int i = 0; i < 3; ++i)
-            if (arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2])
+            if (arr[i][0] != ' ' && arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2])
             {
                 if (arr[i][0] == 'X')
                 {
-                    cout << "Player 1 winer" << endl;
+                    cout << "Player 1 winer\n";
                     return true;
                 }
                 else if (arr[i][0] == 'O')
                 {
-                    cout << "Player 2 winer" << endl;
+                    cout << "Player 2 winer\n";
                     return true;
                 }
             }
 
         for (int j = 0; j < 3; ++j)
-            if (arr[0][j] == arr[1][j] && arr[1][j] == arr[2][j])
+            if (arr[0][j] != ' ' && arr[0][j] == arr[1][j] && arr[1][j] == arr[2][j])
             {
                 if (arr[0][j] == 'X')
                 {
-                    cout << "Player 1 winer" << endl;
+                    cout << "Player 1 winer\n";
                     return true;
                 }
                 else if (arr[0][j] == 'O')
                 {
-                    cout << "Player 2 winer" << endl;
+                    cout << "Player 2 winer\n";
                     return true;
                 }
             }
@@ -72,12 +71,12 @@ public:
         {
             if (arr[0][0] == 'X')
             {
-                cout << "Player 1 winer" << endl;
+                cout << "Player 1 winer\n";
                 return true;
             }
             else if (arr[0][0] == 'O')
             {
-                cout << "Player 2 winer" << endl;
+                cout << "Player 2 winer\n";
                 return true;
             }
         }
@@ -86,12 +85,12 @@ public:
         {
             if (arr[0][2] == 'X')
             {
-                cout << "Player 1 winer" << endl;
+                cout << "Player 1 winer\n";
                 return true;
             }
             else if (arr[0][2] == 'O')
             {
-                cout << "Player 2 winer" << endl;
+                cout << "Player 2 winer\n";
                 return true;
             }
         }
@@ -99,23 +98,22 @@ public:
     }
     bool isFull()
     {
-        Dooz d;
-        if (counter > 9)
+        if (counter >= 9)
         {
             cout << "All pLaces is full";
-            d.play();
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
     void drawBoard()
     {
+        cout << "\n";
         for (int i = 0; i < 3; i++)
         {
-            cout << "-------";
+            cout << "-------\n";
             cout << "|" << arr[i][0] << "|" << arr[i][1] << "|" << arr[i][2] << "|\n";
-            cout << "-------";
         }
+        cout << "-------\n";
     }
     void isValidMove(char c)
     {
@@ -140,49 +138,71 @@ public:
     void makeMove(char c, int i, int j)
     {
         arr[i][j] = c;
+        addcounter();
     }
 };
 
 class Dooz
 {
 private:
-    Board b;
+    Board *b;
     int appointment = 1;
 
 public:
+    Dooz(Board *board)
+    {
+        b = board;
+    }
     void manageappointment()
     {
         if (appointment == 1)
         {
-            b.isValidMove('X');
+            b->isValidMove('X');
         }
         else if (appointment == 2)
         {
-            b.isValidMove('O');
+            b->isValidMove('O');
         }
         appointment = (appointment == 1) ? 2 : 1;
     }
-    bool play()
+    void play()
     {
         cout << "The game is equal";
-        return true;
+        return;
     }
 };
 
 int main()
 {
+    Board b;
+    Dooz d(&b);
     string name1, name2;
     cout << "Please enter the name of player 1:\n";
     cin >> name1;
-    cout << "Your sign as a player 1 is (X)";
+    cout << "Your sign as a player 1 is (X)\n";
     cout << "Please enter the name of player 2:\n";
     cin >> name2;
-    cout << "Your sign as a player 1 is (O)";
+    cout << "Your sign as a player 2 is (O)\n";
 
     Player p1(name1, 'X');
     Player p2(name2, 'O');
 
-
-
+    while (true)
+    {
+        b.drawBoard();
+        d.manageappointment();
+        if (b.checkWin())
+        {
+            b.drawBoard();
+            cout << "The Dooz ended\n";
+            break;
+        }
+        if (b.isFull())
+        {
+            d.play();
+            cout << "The Dooz ended. the game have not winer\n";
+            break;
+        }
+    }
     return 0;
 }
