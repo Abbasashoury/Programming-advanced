0e612bbc179b8b16d37dc7496522b1135aa00117
Abbas Ashoury <abbasashoury84@gmail.com>
2025-12-11 21:47:44 +0330

    primary structure 2 of T&RMSystem

--- Code/Project/miniproject/miniproject4/T&RMSystem.cpp
+++ Code/Project/miniproject/miniproject4/T&RMSystem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,36 +12,58 @@ private:
 public:
     Time(int h = 0, int m = 0) : hour(h), minute(m) {}
 
-    int toHour() const { return hour; }
-    int toMinutes() const { return minute; }
+    int gethour() const { return hour; }
+    int toMinutes() const { return 60 * hour + minute; }
 
-    bool operator<(const Time &other) const
+    bool operator>(const Time &other) const
     {
-        return this->toMinutes() < other.toMinutes();
+        return this->toMinutes() > other.toMinutes();
     }
     bool operator==(const Time &other) const
     {
-        return this->toHour() == other.toHour() && this->toMinutes() == other.toMinutes();
+        return this->gethour() == other.gethour() && this->toMinutes() == other.toMinutes();
     }
-    bool operator-(const Time &other) const
+    int operator-(const Time &other) const
     {
         return this->toMinutes() - other.toMinutes();
     }
 };
-class TimeEnterval
+class TimeInterval
 {
 private:
+    int timestart;
+    int timeend;
+
 public:
+    bool overlaps(const TimeInterval &) {}
+    int duration() {}
+    TimeInterval merge(const TimeInterval &) {}
+    bool operator+(const TimeInterval &other) const {}
+    bool operator<(const TimeInterval &other) const {}
+    bool operator==(const TimeInterval &other) const {}
 };
 class Resource
 {
 private:
+    int id;
+    string name;
+    vector<TimeInterval> intervals;
+
 public:
+    void addInterval(const TimeInterval &) {}
+    void hasConflict(const TimeInterval &) {}
+    void printSchedule() {}
 };
 class ResourceManager
 {
 private:
+    vector<Resource> resources;
+    static int totalResources;
+
 public:
+    void addResource(const Resource &) {}
+    void searchResourceByName(const string &) {}
+    void printAllSchedules() {}
 };
 
 int main()
